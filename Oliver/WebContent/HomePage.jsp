48f34599db22d0d3e657f7086d1134b531bd205e
OliverLai <1558755856@OliverLai.github.com>
2019-04-01 22:53:19 +0800

    the foundation

---
+++ Oliver/WebContent/HomePage.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" 
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" contentType="text/html; charset=UTF-8">
+<title>Oliver的休息室</title>
+</head>
+<body>
+	<div id = "header" class = "warp">
+		<div id = "logo">
+			Oliver's lounge
+		</div>
+	</div>
+<%
+
+%>
+</body>
+</html>
---
+++ Oliver/src/test00/HomePage.java
@@ -0,0 +1,34 @@
+package test00;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomePage
+ */
+@WebServlet("/HomePage.do")
+public class HomePage extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("nihao");
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
